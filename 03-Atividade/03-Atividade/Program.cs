806a0f925d46a5c582bc5c8b01ab337707ee0a0f
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-03 11:01:23 -0300

    Feat: Atividade 3 no C#, finalizada

---
+++ 03-Atividade/03-Atividade/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+Console.WriteLine("Digite o valor para a base: ");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor para a altura: ");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"A Área do retângulo é de {num1 * num2}");
