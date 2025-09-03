34a5ed370af7f6f7704f71187b2368c47c95f30c
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-03 10:57:24 -0300

    Feat: Atividade 2 no C#, finalizada

---
+++ 02-Atividade/02-Atividade/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+Console.WriteLine("Digite um número: ");
+num1 = int.Parse(Console.ReadLine());  
+Console.WriteLine($"O antessor do {num1} é: {num1 - 1}");   
+
+
