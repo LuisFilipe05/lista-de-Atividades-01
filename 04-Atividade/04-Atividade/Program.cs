250c4827880e04558c3d055cc65aad53bc49b377
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-03 11:20:11 -0300

    Feat: Atividade 04 no C#, finaliada

---
+++ 04-Atividade/04-Atividade/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idadeEmMeses;
+int idadeEmDias;
+int idadeEmAnos;
+int totalIdade;
+
+Console.WriteLine("Digite quantos dias se passaram do seu aniversário");
+idadeEmDias = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite quantos meses se passaram do seu anivesário");
+idadeEmMeses = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite quantos anos você tem");
+idadeEmAnos = int.Parse(Console.ReadLine());
+totalIdade = (idadeEmMeses * 30) + (idadeEmAnos * 365) + idadeEmDias;
+Console.WriteLine($"Você tem {idadeEmAnos} anos, {idadeEmMeses} meses e {idadeEmDias} dias");
+Console.WriteLine($"Você tem {totalIdade} dias");
