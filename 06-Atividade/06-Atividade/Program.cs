10d3c75d4b3134360235bbcc28430e8176160faa
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-03 11:54:04 -0300

    Feat: Atividade 6 no C#, finalizada

---
+++ 06-Atividade/06-Atividade/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int salario;
+float reajuste;
+
+Console.WriteLine("Digite o seu salário ");
+salario = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o reajuste que quer fazer no salário");
+reajuste = float.Parse(Console.ReadLine());
+Console.WriteLine($"Seu novo salário de acordo com o reajuste é de {(reajuste / 100 * salario) + salario}");
+
+
