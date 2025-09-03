e1fd1cbf3847e526e34edf206213c96c6daab933
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-03 10:52:33 -0300

    Feat: Alteração de texto

--- 01-Atividade/01-Atividade/Program.cs
+++ 01-Atividade/01-Atividade/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Luis!");
+int num1;
+int num2;
+int num3;
+
+Console.WriteLine("Digite o valor para A é: ");
+num1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Digite o valor para B é: ");
+num2 = int.Parse(Console.ReadLine());
+num3 = num1;
+num1 = num2;
+num2 = num3;   
+Console.WriteLine($"O valor de A é {num1} e o valor de B é {num2}");
+
