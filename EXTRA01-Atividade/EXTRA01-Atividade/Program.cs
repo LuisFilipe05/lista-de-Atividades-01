2ee037b25ef7ec5062ef24a7ccabd366f8e02e8f
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 10:52:39 -0300

    Feat: Atividade extra 1 feita no C#, finalizada

---
+++ EXTRA01-Atividade/EXTRA01-Atividade/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double dinheiro;
+double dolar; 
+Console.WriteLine("Digite quantos reais você tem na carteira!");
+dinheiro = Double.Parse(Console.ReadLine());
+
+dolar = dinheiro / 3.45;
+
+Console.WriteLine($"Você pode comprar {dolar} dolares, com {dinheiro} reais que você tem");
+
+
