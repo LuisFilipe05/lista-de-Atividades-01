4b610917021fc19967a5213bd925899dfe23696b
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 08:11:46 -0300

    Feat: Atividade feita em C#, finalizada

---
+++ 08-Atividade/08-Atividade/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Digite um valor que não seja 0!");
+
+int num1;
+num1 = int.Parse(Console.ReadLine()); 
+if (num1 > 0)
+{
+    Console.WriteLine("O número é positivo!");
+}
+else
+{
+    Console.WriteLine("O número é negativo!");
+}
