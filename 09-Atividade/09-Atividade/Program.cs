886cc04c61e15a5ede7b48abb092965385abce62
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 08:36:16 -0300

    Feat: Atividade 9 feita no C#, finalizada

---
+++ 09-Atividade/09-Atividade/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int maca;
+Console.WriteLine("Digite o número de maçãs compradas!");
+maca = int.Parse(Console.ReadLine());
+
+if (maca >= 12)
+{
+    Console.WriteLine($"Você comprou {maca} unidades, o valor é {1.0 * maca} reais");
+}
+else if (maca <= 11)
+{
+    Console.WriteLine($"Você comprou {maca} unidades, o valor é {1.3 * maca} reais");
+}
