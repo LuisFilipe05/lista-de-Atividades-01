4dc8a926360fa7140631023f99b6f840171109a2
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 11:26:05 -0300

    Feat: Atividade extra 5 feita no C#, finalizada

---
+++ EXTTRA05-Atividade/EXTTRA05-Atividade/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+double salario;
+
+Console.WriteLine("Digite o salário para ter o aumento de 15%");
+salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Seu novo slaário é de {(salario * 1.15) + salario} reais");
