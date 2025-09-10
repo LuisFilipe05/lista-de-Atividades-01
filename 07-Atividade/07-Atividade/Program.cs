8a6d966d6ad80d0fff92023c518ecc7a42b8e6d3
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 07:59:02 -0300

    Feat: Atividade feita no C#, finalizada

---
+++ 07-Atividade/07-Atividade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Digite um valor!");
+int num1;
+
+num1 = int.Parse(Console.ReadLine());
+if (num1 > 10)
+{
+    Console.WriteLine("O número digitado é maior que 10!");
+}
+else
+{
+    Console.WriteLine("O número digitado é menor que 10!");
+}
+
+
