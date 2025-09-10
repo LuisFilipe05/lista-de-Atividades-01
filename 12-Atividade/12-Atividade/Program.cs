54c0c74e0af38a055617079a2feb1d19a606531e
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 09:13:12 -0300

    Feat: Atividade 12 feita no C#, finalizada

--- 12-Atividade/12-Atividade/Program.cs
+++ 12-Atividade/12-Atividade/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int num1, num2;
+Console.WriteLine("Digite o primeiro valor, sabendo que ele não pode ser igual o segundo valor!");
+num1 = int. Parse( Console.ReadLine() );
+
+Console.WriteLine("Digite o segundo valor, sabendo que ele não pode ser igual o primeiro valor!");
+num2 = int. Parse( Console.ReadLine() );       
+
+if ( num1 >= num2 )
+{
+    Console.WriteLine($"O primeiro valor é maior que o segundo valor!");
+}
+else
+{
+    Console.WriteLine($"O segundo valor é maior que o primeiro valor!");
+}
