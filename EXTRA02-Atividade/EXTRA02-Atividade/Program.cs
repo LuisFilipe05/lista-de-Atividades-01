576e772693cbd9b19052f82c85fc90a39e2b6be0
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 11:00:57 -0300

    Feat: Atividade extra 3 feita no C#, finalizada

---
+++ EXTRA02-Atividade/EXTRA02-Atividade/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double altura, largura, area;
+
+Console.WriteLine("Digite a altura da parede");
+altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a largura da parede");
+largura = double.Parse(Console.ReadLine());
+
+area = largura * altura;
+Console.WriteLine($"A quantidade de tinta necessária para pintar a parede é {area / 2} litros");
