f56507d3599d122855997c08dca988f848b6e90d
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 11:21:37 -0300

    Feat: Atividade extra 4 feita no C#, finalizada

---
+++ EXTRA04-Atividade/EXTRA04-Atividade/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double preco, precoPromocional; 
+
+Console.WriteLine("Digite o preço do produto");
+preco = double.Parse(Console.ReadLine());
+
+precoPromocional = preco * 0.05;
+
+Console.WriteLine($"O novo preço do produto é {preco - precoPromocional} reais");
