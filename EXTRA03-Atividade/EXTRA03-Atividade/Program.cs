daa06b0a167b309b4d47ecfe9ed66fcf431988b9
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 11:08:36 -0300

    Feat: Atvidade extra 3 feita no C#, finalizada

---
+++ EXTRA03-Atividade/EXTRA03-Atividade/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b, c, delta; 
+
+Console.WriteLine("Digite o valor de A");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C");
+c = double.Parse(Console.ReadLine());
+
+delta = (b * b) - 4 * a * c;
+
+Console.WriteLine($"O valor de delta é {delta}");
