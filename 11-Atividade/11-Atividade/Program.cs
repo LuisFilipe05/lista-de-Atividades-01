b12b7266c408589531e078bfcb64692061850973
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 09:07:49 -0300

    Feat: Atividade 11 feita no C#, finalizada

--- 11-Atividade/11-Atividade/Program.cs
+++ 11-Atividade/11-Atividade/Program.cs
@@ -1,19 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int ano1;
-int ano2;
+int anoAtual  = 2025;
+int anoNascimento;
 int anoVoto;
 Console.WriteLine("Digite o ano atual!");
-ano1 = int. Parse(Console.ReadLine());
+anoAtual = int. Parse(Console.ReadLine());
 
 Console.WriteLine("Digite o ano em que você nasceu!");
-ano2 = int.Parse(Console.ReadLine());
+anoNascimento = int.Parse(Console.ReadLine());
 
-anoVoto = ano1 - ano2;
-if (anoVoto >= 18 )
+anoVoto = anoAtual - anoNascimento;
+if (anoVoto >= 18)
 {
     Console.WriteLine($"Você tem {anoVoto} anos, sendo assim, você é obrigado a votar!");
 }
-else
+else if (anoVoto <= 16)
 {
     Console.WriteLine($"Você tem {anoVoto} anos, sendo assim, você não é obrigado a votar!");
+}
+else
+{
+    Console.WriteLine($"Você tem {anoVoto} anos, sendo assim, seu voto é facultativo!");
 }
