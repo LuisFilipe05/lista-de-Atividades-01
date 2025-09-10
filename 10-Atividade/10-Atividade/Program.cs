afbfe8ed9ff720f3f42a8c727edd26641b916eb9
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-10 08:47:32 -0300

    Feat: Atividade 10 feita no C#, finalizada

---
+++ 10-Atividade/10-Atividade/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int nota1;
+int nota2;
+int media;
+Console.WriteLine("Digite o valor da sua primeira nota!");
+nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da sua segunda nota!");
+nota2 = int.Parse(Console.ReadLine());
+media = (nota1 + nota2) /2;
+if (media >= 6)
+{
+    Console.WriteLine($"Você foi aprovado com média de {media}");
+}
+else
+{
+    Console.WriteLine($"Você foi reprovado com média de {media}");
+}
