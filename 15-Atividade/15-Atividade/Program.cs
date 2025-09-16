3a905c984f140d34e3c66eaeab5236be8e3f929d
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-16 08:41:26 -0300

    Feat: Atividade 15 feita no C#, finalizada

---
+++ 15-Atividade/15-Atividade/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero;
+Console.WriteLine("Irei conferir os numeros de 1000 a 2000 e também os quando dividido por 11, tem resto igual a 5");
+int contador;
+for (contador = 1000; contador <= 2000; contador++)
+{
+    if (contador % 11 == 5)
+    {
+        Console.WriteLine($"{contador}");
+    }
+}
