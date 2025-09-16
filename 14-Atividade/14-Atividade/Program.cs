e4f943e8f4b3ce9af55ec48c901a804962d990f3
Luis Filipe dos Santos <luissesies@gmail.com>
2025-09-16 08:25:46 -0300

    Feat: Atividade extra 14 feita no C#, finalizada

---
+++ 14-Atividade/14-Atividade/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero, valor;
+Console.WriteLine("Digite um numero e te mostrarei todos os numros inteiros até chegar nele!");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"Os numeros entre 1 a {numero} são:");
+
+int contador = 1;
+while (contador <= numero)
+{
+    Console.WriteLine($"{contador}");
+    contador++;
+}
+
